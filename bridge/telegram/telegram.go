b989f799ab12fd6b36f1eb6e4a6a65946d40ed59
nathan <nathan@zyr.io>
2017-08-23 01:47:36 +0100

    Add ignore command

--- bridge/telegram/telegram.go
+++ bridge/telegram/telegram.go
@@ -1,77 +1,79 @@
 package btelegram
 
 import (
-    "github.com/Subluminal/matterbridge/bridge/config"
-    log "github.com/Sirupsen/logrus"
-    "github.com/go-telegram-bot-api/telegram-bot-api"
-    "strconv"
-    "html"
+	"html"
+	"strconv"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/Subluminal/matterbridge/bridge/config"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 type Btelegram struct {
-    c       *tgbotapi.BotAPI
-    Config  *config.Protocol
-    Remote  chan config.Message
-    Account string
+	c       *tgbotapi.BotAPI
+	Config  *config.Protocol
+	Remote  chan config.Message
+	Account string
 }
 
 var flog *log.Entry
 var protocol = "telegram"
 
 func init() {
-    flog = log.WithFields(log.Fields{"module": protocol})
+	flog = log.WithFields(log.Fields{"module": protocol})
 }
 
 func New(cfg config.Protocol, account string, c chan config.Message) *Btelegram {
-    b := &Btelegram{}
-    b.Config = &cfg
-    b.Remote = c
-    b.Account = account
-    return b
+	b := &Btelegram{}
+	b.Config = &cfg
+	b.Remote = c
+	b.Account = account
+	return b
 }
 
 func (b *Btelegram) Connect() error {
-    var err error
-    flog.Info("Connecting")
-    b.c, err = tgbotapi.NewBotAPI(b.Config.Token)
-    if err != nil {
-        flog.Debugf("%#v", err)
-        return err
-    }
-    updates, err := b.c.GetUpdatesChan(tgbotapi.NewUpdate(0))
-    if err != nil {
-        flog.Debugf("%#v", err)
-        return err
-    }
-    flog.Info("Connection succeeded")
-    go b.handleRecv(updates)
-    return nil
+	var err error
+	flog.Info("Connecting")
+	b.c, err = tgbotapi.NewBotAPI(b.Config.Token)
+	if err != nil {
+		flog.Debugf("%#v", err)
+		return err
+	}
+	updates, err := b.c.GetUpdatesChan(tgbotapi.NewUpdate(0))
+	if err != nil {
+		flog.Debugf("%#v", err)
+		return err
+	}
+	flog.Info("Connection succeeded")
+	go b.handleRecv(updates)
+	return nil
 }
 
 func (b *Btelegram) JoinChannel(channel string) error {
-    return nil
+	return nil
 }
 
 func (b *Btelegram) Send(msg config.Message) error {
-    flog.Debugf("Receiving %#v", msg)
-    chatid, err := strconv.ParseInt(msg.Channel, 10, 64)
-    if err != nil {
-        return err
-    }
-    m := tgbotapi.NewMessage(chatid, "<b>"+msg.Username+"</b>: "+html.EscapeString(msg.Text))
-    m.ParseMode = "html"
-    _, err = b.c.Send(m)
-    return err
+	flog.Debugf("Receiving %#v", msg)
+	chatid, err := strconv.ParseInt(msg.Channel, 10, 64)
+	if err != nil {
+		return err
+	}
+	m := tgbotapi.NewMessage(chatid, "<b>"+msg.Username+"</b>: "+html.EscapeString(msg.Text))
+	m.ParseMode = "html"
+	_, err = b.c.Send(m)
+	return err
 }
 
 func (b *Btelegram) handleRecv(updates <-chan tgbotapi.Update) {
-    flog.Debug("Starting telegram handler")
-    for update := range updates {
-        if update.Message == nil {
-            continue
-        }
-        flog.Debugf("Sending message from %s on %s to gateway", update.Message.From.UserName, b.Account)
-        b.Remote <- config.Message{Username: "\x02@\x02"+update.Message.From.UserName, Text: update.Message.Text, Channel: strconv.FormatInt(update.Message.Chat.ID, 10), Account: b.Account}
+	flog.Debug("Starting telegram handler")
+	for update := range updates {
 
-    }
+		if update.Message.Text == "" {
+			continue
+		}
+		flog.Debugf("telegram: Sending message from %s on %s to gateway", update.Message.From.UserName, b.Account)
+		b.Remote <- config.Message{Username: "\x02@\x02" + update.Message.From.UserName, Text: update.Message.Text, Channel: strconv.FormatInt(update.Message.Chat.ID, 10), Account: b.Account}
+
+	}
 }
